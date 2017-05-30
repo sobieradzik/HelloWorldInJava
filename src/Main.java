4896c10e8f2cd5110ba74d5a4220a227c72bd050
rwajm <rwajm@212.191.89.73>
2017-05-30 15:20:55 +0200

    rw

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello 4 Git by RW");
+		System.out.println("Hello 4 Git by RW version 2");
 	}
 
 }
